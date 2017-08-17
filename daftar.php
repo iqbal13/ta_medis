e39be627ee043d65d106654935b8772786672ccd
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-17 22:23:03 +0700

    file data

---
+++ daftar.php
@@ -0,0 +1,255 @@
+<?php 
+include 'partial/head.php';
+?>
+    <body>
+        <div id="container">
+
+            <div class="content wrapper container">
+                
+            <header>
+        <div class="cols-left logo">
+            <a href="http://gadingpluit-hospital.com/"><img src="http://gadingpluit-hospital.com/assets/images/logo.png"  oncontextmenu="return false;"/></a>
+        </div>
+        <div class="cols-right contact">
+            <!--img src="images/phone.png" align="left"/-->
+            <p>Jl. Boulevard Timur Raya, Kelapa Gading - Jakarta 14250 <br/>T. (+6221) 4521001, 4520201 &nbsp;&nbsp; F. (+6221) 4520578 <br/>E. gadingpluit@gadingpluit-hospital.com </p>
+            <h2>Gawat Darurat: (+6221) 4-5858-258</h2>
+        </div>
+        <div class="clear"></div>
+</header>                <nav>
+                    <ul class="cols-left menu" id="jMenu">
+                        <li><a href="http://gadingpluit-hospital.com/">BERANDA</a></li>
+                                                <li>
+                                                        <a href="#" class="dropdown">Tentang Kami <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Sejarah">Sejarah</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Visi-Misi">Visi & Misi</a>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+                                                <li>
+                                                        <a href="#" class="dropdown">Fasilitas <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Layanan-24-Jam">Layanan 24 Jam</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Rawat-Jalan">Rawat Jalan</a>
+                                                                                <ul>
+                                             
+                                            <li>
+                                                <a href="http://gadingpluit-hospital.com/ind/content/Poliklinik">Poliklinik</a>
+                                            </li>
+                                             
+                                            <li>
+                                                <a href="http://gadingpluit-hospital.com/ind/content/Fisioterapi">Fisioterapi</a>
+                                            </li>
+                                             
+                                            <li>
+                                                <a href="http://gadingpluit-hospital.com/ind/content/Hidroterapi">Hidroterapi</a>
+                                            </li>
+                                                                                    </ul>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Rawat-Inap">Rawat Inap</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Medical-Check-Up">Medical Check-Up</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Penunjang-Diagnostik">Penunjang Diagnostik</a>
+                                                                                <ul>
+                                             
+                                            <li>
+                                                <a href="http://gadingpluit-hospital.com/ind/content/Radiologi">Radiologi</a>
+                                            </li>
+                                             
+                                            <li>
+                                                <a href="http://gadingpluit-hospital.com/ind/content/USG-4D">USG-4D</a>
+                                            </li>
+                                                                                    </ul>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+                                                <li>
+                                                        <a href="#" class="dropdown">Layanan Unggulan <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/PETCT-SCAN">PET/CT-SCAN</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/VERTEBROPLASTY-VESSELPLASTY">VERTEBROPLASTY & VESSELPLASTY</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/BEDAH-ENDO-LAPAROSKOPIK">BEDAH ENDO-LAPAROSKOPIK</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/TACE-TACI">TACE / TACI</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/DSA-BRAIN-SPA">DSA (BRAIN SPA)</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/TERAPI-CRYO">TERAPI CRYO</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/RADIOTERAPI">RADIOTERAPI</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/FERTILITAS-IN-VITRO-IVF">FERTILITAS IN-VITRO ( IVF )</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/MRI-15-TESLA-MAGNETOM-AVANTO-FIT"> MRI 1,5 TESLA MAGNETOM AVANTO FIT</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/MSCT-SCAN-64-SLICE-SOMATOM-SENSATION-CARDIAC-64">MSCT-SCAN 64 SLICE - SOMATOM SENSATION CARDIAC 64 </a>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+                                                <li>
+                                                        <a href="#" class="dropdown">News  <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/ARTIKEL">ARTIKEL</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/BERITA">BERITA</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/promo">PROMO</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/video-galeri">VIDEO GALLERY</a>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+                                                <li>
+                                                        <a href="#" class="dropdown">Jaringan Kami <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/KLINIK-BAERMED">KLINIK BAERMED</a>
+                                                                            </li>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/PERUSAHAAN-REKANAN">PERUSAHAAN REKANAN</a>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+                                                <li>
+                                                        <a href="#" class="dropdown">Medis <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="http://gadingpluit-hospital.com/ind/content/Laporan-Medis">Laporan Medis</a>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+
+
+                                                      <li>
+                                                        <a href="#" class="dropdown">Rawat Jalan <!--span class="arrow-hor"></span--></a>
+                                                            <ul>
+                                     
+                                    <li>
+                                                                                <a href="<?php echo $url ?>daftar.php">Daftar</a>
+                                                                            </li>
+                                                                    </ul>
+                                                    </li>
+
+                                                <div class="clear"></div>
+                    </ul>
+
+                    <div class="clear"></div>
+                </nav>
+        
+
+<div class="row" style="margin-top: 0px !important;">
+<div class="col-md-12" style="padding-right: 5px !important;">
+    <div class="cols-home" id="cols-teal">
+        
+<div id="example-basic">
+    <h3>Keyboard</h3>
+    <section>
+        <p>Try the keyboard navigation by clicking arrow left or right!</p>
+    </section>
+    <h3>Effects</h3>
+    <section>
+        <p>Wonderful transition effects.</p>
+    </section>
+    <h3>Pager</h3>
+    <section>
+        <p>The next and previous buttons help you to navigate through your content.</p>
+    </section>
+</div>
+        <p>
+                 </p>
+        <div class="clear"></div>
+    </div>
+</div>
+
+    </div>
+<div class="clear"></div>
+            </div>
+        </div>
+
+        <footer>
+    <ul>
+        <li id="logo-footer" class="cols-left col-md-5">
+            <h1><SPAN>Rumah Sakit</SPAN> GADING PLUIT</h1>
+            <img src="http://gadingpluit-hospital.com/assets/images/logo-footer-ind.png"/>
+
+        </li>
+        <li id="copyright" class="col-md-3"><span>Copyright © 2013 - All Right Reserved</span></li>
+        <li id="links" class="cols-right">
+            <a href="http://gadingpluit-hospital.com/ind/lokasi">LOKASI</a>
+            <a href="http://gadingpluit-hospital.com/ind/hubungikami">HUBUNGI KAMI</a>
+            <a href="http://gadingpluit-hospital.com/ind/content/lowongan">LOWONGAN</a>
+            <br class="clear"/>
+        </li>
+        
+        <li id="alamat-footer" class="col-md-5">
+            <p>Jl. Boulevard Timur Raya, Kelapa Gading - Jakarta 14250 <br/>
+			T. (+6221) 4521001, 4520201 &nbsp;&nbsp; F. (+6221)  4520578 <br/>
+			E. gadingpluit@gadingpluit-hospital.com </p>
+            <h1>Gawat Darurat: (+6221) 4-5858-258</h1>
+        </li>
+        <li id="links-mobile" class="col-md-5">
+            <a href="http://gadingpluit-hospital.com/ind/lokasi">LOKASI</a>
+            <a href="http://gadingpluit-hospital.com/ind/hubungikami">HUBUNGI KAMI</a>
+            <a href="http://gadingpluit-hospital.com/ind/content/lowongan">LOWONGAN</a>
+            <p>Copyright © 2013 - All Right Reserved</p>
+        </li>  
+        
+        <br class="clear"/>
+    </ul>
+</footer>    </body>
+</html>
+
+<script src="http://gadingpluit-hospital.com/assets/bootstrap/bootstrap.min.js"></script> 
+<script src="<?php echo $url ?>js/main.js"></script>
+<script src="http://gadingpluit-hospital.com/assets/bootstrap/jquery.mobilemenu.js" type="text/javascript"></script>
