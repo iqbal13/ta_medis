9885f76e7b57fa243c36ef61485cc0965c7739cf
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:25:50 +0700

    add menu admijn

---
+++ partial/menu_admin.php
@@ -0,0 +1,8 @@
+   <ul class="cols-left menu" id="jMenu">
+    <li> <a href="<?php echo $url ?>index_admin.php" class="dropdown">Home Admin  <!--span class="arrow-hor"></span--></a> </li>
+    <li> <a href="<?php echo $url ?>data_pasien.php"> Data Pasien </a> </li>
+    <li> <a href="<?php echo $url ?>data_registrasi.php"> Data Registrasi </a> </li>
+    <li> <a href="<?php echo $url ?>data_dokter.php"> Data Dokter </a> </li>
+    <li> <a href="<?php echo $url ?>data_user.php"> Data User </a> </li>
+    <li> <a href="<?php echo $url ?>logout.php"> Keluar </a> </li>
+    </ul>
