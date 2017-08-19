7b9411dadcaa906e80b503832bae08831127b539
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:25:24 +0700

    add footer

---
+++ partial/footer.php
@@ -0,0 +1,31 @@
+   <footer>
+    <ul>
+        <li id="logo-footer" class="cols-left col-md-5">
+            <h1><SPAN>Rumah Sakit</SPAN> GADING PLUIT</h1>
+            <img src="http://gadingpluit-hospital.com/assets/images/logo-footer-ind.png"/>
+
+        </li>
+        <li id="copyright" class="col-md-3"><span>Copyright © 2013 - All Right Reserved</span></li>
+        <li id="links" class="cols-right">
+            <a href="http://gadingpluit-hospital.com/ind/lokasi">LOKASI</a>
+            <a href="http://gadingpluit-hospital.com/ind/hubungikami">HUBUNGI KAMI</a>
+            <a href="http://gadingpluit-hospital.com/ind/content/lowongan">LOWONGAN</a>
+            <br class="clear"/>
+        </li>
+        
+        <li id="alamat-footer" class="col-md-5">
+            <p>Jl. Boulevard Timur Raya, Kelapa Gading - Jakarta 14250 <br/>
+			T. (+6221) 4521001, 4520201 &nbsp;&nbsp; F. (+6221)  4520578 <br/>
+			E. gadingpluit@gadingpluit-hospital.com </p>
+            <h1>Gawat Darurat: (+6221) 4-5858-258</h1>
+        </li>
+        <li id="links-mobile" class="col-md-5">
+            <a href="http://gadingpluit-hospital.com/ind/lokasi">LOKASI</a>
+            <a href="http://gadingpluit-hospital.com/ind/hubungikami">HUBUNGI KAMI</a>
+            <a href="http://gadingpluit-hospital.com/ind/content/lowongan">LOWONGAN</a>
+            <p>Copyright © 2013 - All Right Reserved</p>
+        </li>  
+        
+        <br class="clear"/>
+    </ul>
+</footer>  
