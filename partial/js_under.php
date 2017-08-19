98310366a72cd3bbe5387b704fcdf72733d124c3
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:25:56 +0700

    js_under

---
+++ partial/js_under.php
@@ -0,0 +1,4 @@
+
+<script src="http://gadingpluit-hospital.com/assets/bootstrap/bootstrap.min.js"></script> 
+<script src="http://gadingpluit-hospital.com/assets/bootstrap/main.js"></script>
+<script src="http://gadingpluit-hospital.com/assets/bootstrap/jquery.mobilemenu.js" type="text/javascript"></script>
