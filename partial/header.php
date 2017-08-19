d699f63541a04f157de9f6516e0a5272f229b1be
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:25:31 +0700

    add header

---
+++ partial/header.php
@@ -0,0 +1,21 @@
+    
+            <header>
+        <div class="cols-left logo">
+            <a href="http://gadingpluit-hospital.com/"><img src="http://gadingpluit-hospital.com/assets/images/logo.png"  oncontextmenu="return false;"/></a>
+        </div>
+        <div class="clear clear-mobile"></div>
+        <div id="language-mobile">
+            <a href="http://gadingpluit-hospital.com/eng"><img src="http://gadingpluit-hospital.com/assets/images/eng.jpg"/>English</a> | 
+            <a href="http://gadingpluit-hospital.com/ind"><img src="http://gadingpluit-hospital.com/assets/images/ind.jpg"/>Bahasa</a>
+        </div>
+        <div class="cols-right contact">
+            <!--img src="images/phone.png" align="left"/-->
+            <div id="language">
+                <a href="http://gadingpluit-hospital.com/eng"><img src="http://gadingpluit-hospital.com/assets/images/eng.jpg"/>English</a> | 
+                <a href="http://gadingpluit-hospital.com/ind"><img src="http://gadingpluit-hospital.com/assets/images/ind.jpg"/>Bahasa</a>
+            </div>
+            <p>Jl. Boulevard Timur Raya, Kelapa Gading - Jakarta 14250 <br/>T. (+6221) 4521001, 4520201 &nbsp;&nbsp; F. (+6221) 4520578 <br/>E. gadingpluit@gadingpluit-hospital.com </p>
+            <h2>Gawat Darurat: (+6221) 4-5858-258</h2>
+        </div>
+        <div class="clear"></div>
+</header>              
