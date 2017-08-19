df3b1eb854f856bc67e5dfba38e57afdfb23cebf
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:57:12 +0700

    proses daftar

--- proses.php
+++ proses.php
@@ -3,18 +3,22 @@
 
 
 $jenis_pasien = $_POST['jenis_pasien'];
-
-if($jenis_pasien == 2){
-
-	//ini buat pasien baru
-	$nomor_rekam = $_POST['nomor_rekam'];
+$nomor_rekam = $_POST['nomor_rekam'];
 	$nama_pasien = $_POST['nama_pasien'];
 	$ttl = $_POST['ttl'];
 	$alamat = $_POST['alamat'];
 	$nomor_kontak  = $_POST['nomor_kontak'];
 
+	$tanggal_lahir = explode("-",$ttl);
+	$tahun_lahir = $tanggal_lahir[0];
 
-	$daftar_query = "INSERT INTO pasien (nomor_rekam,nama_pasien,ttl,alamat,nomor_kontak) VALUES ('$nomor_rekam','$nama_pasien','$ttl','$alamat','$nomor_kontak')";
+	$umur = date('Y') -  $tahun_lahir;
+if($jenis_pasien == 2){
+
+	//ini buat pasien baru
+	
+
+	$daftar_query = "INSERT INTO pasien (nomor_rekam,nama_pasien,ttl,umur,alamat,nomor_kontak) VALUES ('$nomor_rekam','$nama_pasien','$ttl','$umur','$alamat','$nomor_kontak')";
 	$run_query = mysqli_query($db,$daftar_query);
 }else{
 
@@ -45,23 +49,38 @@ if($jenis_pasien == 2){
 	$waktu_registrasi = date('Y-m-d H:i:s');
 
 
-	$query_ins = "INSERT INTO registrasi_rawatjalan (nomor_registrasi,nomor_rekam_medis,tanggal_kunjungan,id_dokter,id_waktu,waktu_registrasi) VALUES ('$no_registrasi_baru','$nomor_rekam','$tanggal_kunjungan','$id_dokter','$id_waktu','$waktu_registrasi')";
+	$query_ins = "INSERT INTO registrasi_rawatjalan (nomor_registrasi,nomor_rekam_medis,tanggal_kunjungan,id_dokter,id_waktu,waktu_registrasi,cara_bayar) VALUES ('$no_registrasi_baru','$nomor_rekam','$tanggal_kunjungan','$id_dokter','$id_waktu','$waktu_registrasi','$cara_bayar')";
 
 	$run_ins = mysqli_query($db,$query_ins);
 	if($run_ins){
 		echo "<script>$('.sw-btn-next').click() </script>";
 		echo "<script>$('.sw-btn-prev').hide() </script>";
 		
+		$dt = mysqli_fetch_array(mysqli_query($db,"SELECT * FROM registrasi_rawatjalan a LEFT JOIN pasien b ON a.nomor_rekam_medis = b.nomor_rekam LEFT JOIN dokter c ON a.id_dokter = c.id_dokter LEFT JOIN poli d ON c.kode_poli = d.kode_poli LEFT JOIN waktu_praktik e ON a.id_waktu = e.id_waktu WHERE a.nomor_registrasi = '$no_registrasi_baru'"));
+		$poli = $dt['nama_poli'];
+		$dokter = $dt['nama_dokter'];
+		$waktu = $dt['waktu_praktik'];
+
 		?>
 
 		<div class="panel-heading">Pendaftaran Berhasil </div>
+		<p> Download data anda sebagai bukti bla bla bla 
+                       <a href="<?php echo $url ?>lapregister.php?nomor_registrasi=<?php echo $no_registrasi_baru ?>" target="_blank"> Download </a>
+           </p>
                         <table class="table" id="data-nyaisi">
                             <tbody>
                             	<tr> <th> No Registrasi : </th> <td> <?php echo $no_registrasi_baru ?> </td> </tr>
                                 <tr> <th>Name:</th> <td> <?php echo $nama_pasien ?></td> </tr>
                                 <tr> <th>Nomor Rekam :</th> <td><?php echo $nomor_rekam ?></td> </tr>
+                                <tr> <th> Umur : </th> <td> <?php echo $umur ?> </td> </tr>
+                                <tr> <th> Poli : </th> <td> <?php echo $poli ?> </td> </tr>
+                                <tr> <th> Dokter : </th> <td>  <?php echo $dokter ?> </td> </tr>
+                                <tr> <th> Waktu : </th> <td> <?php echo $waktu ?> </td> </tr>
+                                <tr> <th> Cara Bayar : </th> <td> <?php echo $cara_bayar ?> </td> </tr>
                             </tbody>
                         </table>
+
+
 
 		<?php
 	}else{
