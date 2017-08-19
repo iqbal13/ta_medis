3a8de5dbd814676e09b3d7a7d77fcc326becdc1f
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:27:35 +0700

    index mobil

---
+++ index_admin.php
@@ -0,0 +1,32 @@
+<?php 
+include 'partial/head.php';
+?>
+    <body oncontextmenu="return false;">
+        <div id="container">
+
+            <div class="content wrapper container">
+            <?php include 'partial/header.php' ?> 
+ <nav>
+                 <?php include 'partial/menu_admin.php' ?>
+                    <div class="clear"></div>
+                </nav>
+                
+              
+
+<div class="row" style="margin-top: 0px !important;">
+<div class="col-md-12" style="margin-bottom: 400px;">
+<h1 class="text-center"> Selamat Datang di halaman administrator </h1>
+</div>
+<div class="clearfix"> </div>
+
+
+
+    </div>
+<div class="clear"></div>
+            </div>
+        </div>
+
+        <?php include 'partial/footer.php' ?>
+       </body>
+</html>
+<?php include 'partial/js_under.php'; ?>
