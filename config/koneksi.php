fd0748ce2b9cab816235e4527dfdb81daf4f0db7
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-08-19 09:59:02 +0700

    ed

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,6 +1,7 @@
 <?php 
-
-	$url = "http://localhost:999/ta1/";
+	$folder = "ta1";
+	$link = "localhost:999";
+	$url = "http://".$link."/".$folder."/";
 
 	$db = mysqli_connect("localhost","root","","ta_medis");
 	session_start();
